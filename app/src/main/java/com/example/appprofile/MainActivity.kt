cf5216ea3ca5710df323a3f6537e31ef3d89c5ac
Kordeliia <lalvmun@hotmail.com>
2023-09-22 14:45:08 +0200

    Commit latitud y longitude

--- app/src/main/java/com/example/appprofile/MainActivity.kt
+++ app/src/main/java/com/example/appprofile/MainActivity.kt
@@ -6,9 +6,28 @@ import com.example.appprofile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var lat : Double = 0.0
+    private var long: Double = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        updateUI()
+        binding.tvLocation.setOnClickListener {
+            binding.tvLocation.text = "Lat: $lat Long: $long"
+        }
+
+    }
+
+    private fun updateUI(name: String = "Laura Alvarez Muñoz",
+                         email: String = "lalvmun@gmail.com",
+                         website : String ="miwebsite.nunchakita.com",
+                         phone : String = "+34622321232") {
+        binding.tvName.text = name
+        binding.tvEmail.text = email
+        binding.tvWebsite.text = website
+        binding.tvPhone.text = phone
+        lat = 37.7448
+        long = -1.1951
     }
 }
